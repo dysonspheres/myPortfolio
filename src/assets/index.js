0a14e5560f5306c7f8e721228741143bc5d092aa
Gregory Zachariah <86430288+dysonspheres@users.noreply.github.com>
2023-05-31 17:44:27 -0500

    Add files via upload

---
+++ src/assets/index.js
@@ -0,0 +1,91 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+import cplus from "./tech/c++.png";
+import java from "./tech/java.svg";
+import openCV from "./tech/openCV.png";
+import pyTorch from "./tech/pytorch.png";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import uh from "./company/uh.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+import utd from "./company/utd.png";
+import convergent from "./company/convergent.png";
+import ut from "./company/ut.png";
+import cognizant from "./company/cognizant.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+import givingTree from "./givingTree.png";
+import carpet from "./carpet.png";
+import door from "./door.png";
+import utcs from "./utcs.jpg";
+import scc from "./scc.png";
+
+export {
+  logo,
+  backend,
+  scc,
+  door,
+  utcs,
+  creator,
+  carpet,
+  mobile,
+  web,
+  github,
+  givingTree,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  cplus,
+  java, 
+  pyTorch,
+  openCV,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  uh,
+  ut,
+  cognizant,
+  utd,
+  convergent,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+};
